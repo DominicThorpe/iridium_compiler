d73fbd2b2d81987f65408922d1f2b82156bd1c14
thorped1 <dompthorpe123@gmail.com>
2023-02-04 17:18:20 +0000

    Added support for unary operators in expressions

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate pest_derive;
 
 
 fn main() {
-    let unparsed_file = std::fs::read_to_string("test_programs/basic_return.c").expect("cannot read C file");
+    let unparsed_file = std::fs::read_to_string("test_programs/unary_operators/valid/nested_ops.c").expect("cannot read C file");
     let ast = parser::parse(&unparsed_file).unwrap();
     println!("{:#?}", ast);
 }
--- src/parser.rs
+++ src/parser.rs
@@ -11,10 +11,27 @@ pub struct CParser;
 
 
 #[derive(Debug, Clone)]
+pub enum UnaryOperator {
+    Negation,
+    Complement,
+    LogicalNeg,
+}
+
+
+#[derive(Debug, Clone)]
 pub enum ASTNode {
     Integer(i16),
     ReturnStatement(Box<ASTNode>),
-    Function(String, String, Vec<ASTNode>),
+    Expression {
+        operator: Option<UnaryOperator>, 
+        expression: Option<Box<ASTNode>>,
+        literal: Option<Box<ASTNode>>
+    },
+    Function {
+        return_type: String,
+        identifier: String,
+        statements: Vec<ASTNode>
+    },
 }
 
 
@@ -26,15 +43,58 @@ fn convert_str_to_int(val:&str) -> i16 {
     } else {
         str::parse::<i16>(val).unwrap().try_into().unwrap()
     }
-} 
+}
 
 
-pub fn build_ast_from_stmt(pair: pest::iterators::Pair<Rule>) -> ASTNode {
+fn get_node_from_unary_operator(operator:&str) -> UnaryOperator {
+    match operator {
+        "-" => UnaryOperator::Negation,
+        "!" => UnaryOperator::LogicalNeg,
+        "~" => UnaryOperator::Complement,
+        _ => panic!("{} is not a recongised unary operator!", operator)
+    }
+}
+
+
+fn build_ast_from_expression(pair: pest::iterators::Pair<Rule>) -> ASTNode {
+    match pair.as_rule() {
+        Rule::expression => {
+            let mut pair = pair.into_inner();
+            let operator_or_literal = pair.next().unwrap();
+            match operator_or_literal.as_rule() {
+                Rule::unary_operator => {
+                    let operator = get_node_from_unary_operator(operator_or_literal.as_str());
+                    let sub_expression = build_ast_from_expression(pair.next().unwrap());
+                    ASTNode::Expression {
+                        operator: Some(operator),
+                        expression: Some(Box::new(sub_expression)),
+                        literal: None
+                    }
+                },
+
+                Rule::number=> {
+                    let literal = ASTNode::Integer(convert_str_to_int(operator_or_literal.as_str()));
+                    ASTNode::Expression {
+                        operator: None,
+                        expression: None,
+                        literal: Some(Box::new(literal))
+                    }
+                },
+
+                unknown_expr => panic!("Unexpected expression: {:?}", unknown_expr),
+            }
+        },
+        unknown_expr => panic!("Unexpected expression: {:?}", unknown_expr),
+    }
+}
+
+
+fn build_ast_from_stmt(pair: pest::iterators::Pair<Rule>) -> ASTNode {
     match pair.as_rule() {
         Rule::return_stmt => {
             let mut pair = pair.into_inner();
-            let value = pair.next().unwrap().as_str();
-            ASTNode::ReturnStatement(Box::new(ASTNode::Integer(convert_str_to_int(value))))
+            let expression = pair.next().unwrap();
+            ASTNode::ReturnStatement(Box::new(build_ast_from_expression(expression)))
         },
         unknown_expr => panic!("Unexpected statement construct: {:?}", unknown_expr),
     }
@@ -62,7 +122,11 @@ pub fn build_ast_from_toplvl(pair: pest::iterators::Pair<Rule>) -> ASTNode {
                 }
             }
 
-            ASTNode::Function(datatype, identifier, statements)
+            ASTNode::Function {
+                return_type: datatype, 
+                identifier: identifier, 
+                statements: statements
+            }
         },
 
         unknown_expr => panic!("Unexpected toplevel construct: {:?}", unknown_expr),
@@ -128,19 +192,19 @@ mod tests {
 
     #[test]
     fn test_parse_basic_program() {
-        let valid_prog_names:Vec<_> = read_dir("test_programs/valid").unwrap()
-                                                                     .map(|dir| dir.unwrap().path())
-                                                                     .into_iter()
-                                                                     .collect();
+        let valid_prog_names:Vec<_> = read_dir("test_programs/return_int_literal/valid")
+                            .unwrap()
+                            .map(|dir| dir.unwrap().path())
+                            .into_iter()
+                            .collect();
         for path in valid_prog_names {
-            println!("Twat: {:?}", path);
             let unparsed_file = std::fs::read_to_string(path.clone()).expect(&format!("Cannot read C file {:?}", path));
             let ast = parser::parse(&unparsed_file).unwrap();
             match ast[0].clone() {
-                parser::ASTNode::Function(return_type, identifier, stmts) => {
+                parser::ASTNode::Function { return_type, identifier, statements } => {
                     assert_eq!(return_type, "int");
                     assert_eq!(identifier, "main");
-                    assert_eq!(stmts.len(), 1);
+                    assert_eq!(statements.len(), 1);
                 },
 
                 _ => panic!("Node {:?} should be a function", ast[0])
@@ -150,10 +214,40 @@ mod tests {
 
     #[test]
     fn test_basic_invalid_return_programs() {
-        let valid_prog_names:Vec<_> = read_dir("test_programs/invalid").unwrap()
-                                                                     .map(|dir| dir.unwrap().path())
-                                                                     .into_iter()
-                                                                     .collect();
+        let valid_prog_names:Vec<_> = read_dir("test_programs/return_int_literal/invalid")
+                            .unwrap()
+                            .map(|dir| dir.unwrap().path())
+                            .into_iter()
+                            .collect();
+        for path in valid_prog_names {
+            let unparsed_file = std::fs::read_to_string(path.clone()).expect(&format!("Cannot read C file {:?}", path));
+            match parser::parse(&unparsed_file) {
+                Ok(_) => panic!("Invalid C file {} should fail but did not!", path.display()),
+                Err(_) => {}
+            }
+        }
+    }
+
+    #[test]
+    fn test_parse_valid_unary_op_program() {
+        let valid_prog_names:Vec<_> = read_dir("test_programs/unary_operators/valid")
+                            .unwrap()
+                            .map(|dir| dir.unwrap().path())
+                            .into_iter()
+                            .collect();
+        for path in valid_prog_names {
+            let unparsed_file = std::fs::read_to_string(path.clone()).expect(&format!("Cannot read C file {:?}", path));
+            let _ = parser::parse(&unparsed_file).unwrap();
+        }
+    }
+
+    #[test]
+    fn test_parse_invalid_unary_op_program() {
+        let valid_prog_names:Vec<_> = read_dir("test_programs/unary_operators/invalid")
+                            .unwrap()
+                            .map(|dir| dir.unwrap().path())
+                            .into_iter()
+                            .collect();
         for path in valid_prog_names {
             let unparsed_file = std::fs::read_to_string(path.clone()).expect(&format!("Cannot read C file {:?}", path));
             match parser::parse(&unparsed_file) {
