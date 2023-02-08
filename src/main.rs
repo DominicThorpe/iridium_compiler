0533e29088f1c781f76b57dee5d775ab54a3359c
thorped1 <dompthorpe123@gmail.com>
2023-02-08 14:56:18 +0000

    Can now generate left-associative AST expressions

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate pest_derive;
 
 
 fn main() {
-    let unparsed_file = std::fs::read_to_string("test_programs/unary_operators/valid/nested_ops.c").expect("cannot read C file");
+    let unparsed_file = std::fs::read_to_string("test_programs/test.c").expect("cannot read C file");
     let ast = parser::parse(&unparsed_file).unwrap();
     println!("{:#?}", ast);
 }
--- src/parser.rs
+++ src/parser.rs
@@ -19,13 +19,32 @@ pub enum UnaryOperator {
 
 
 #[derive(Debug, Clone)]
+pub enum BinaryOperator {
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+}
+
+
+#[derive(Debug, Clone)]
 pub enum ASTNode {
     Integer(i16),
     ReturnStatement(Box<ASTNode>),
-    Expression {
+    Factor {
+        literal: Option<Box<ASTNode>>,
         operator: Option<UnaryOperator>, 
         expression: Option<Box<ASTNode>>,
-        literal: Option<Box<ASTNode>>
+    },
+    Term {
+        lhs: Box<ASTNode>,
+        operator: Option<BinaryOperator>,
+        rhs: Option<Box<ASTNode>>
+    },
+    Expression {
+        lhs: Box<ASTNode>,
+        operator: Option<BinaryOperator>,
+        rhs: Option<Box<ASTNode>>
     },
     Function {
         return_type: String,
@@ -56,16 +75,28 @@ fn get_node_from_unary_operator(operator:&str) -> UnaryOperator {
 }
 
 
-fn build_ast_from_expression(pair: pest::iterators::Pair<Rule>) -> ASTNode {
+fn get_node_from_binary_operator(operator:&str) -> BinaryOperator {
+    match operator {
+        "+" => BinaryOperator::Add,
+        "-" => BinaryOperator::Subtract,
+        "*" => BinaryOperator::Multiply,
+        "/" => BinaryOperator::Divide,
+        _ => panic!("{} is not a recongised binary operator!", operator)
+    }
+}
+
+
+fn build_ast_from_factor(pair: pest::iterators::Pair<Rule>) -> ASTNode {
     match pair.as_rule() {
-        Rule::expression => {
+        Rule::factor => {
             let mut pair = pair.into_inner();
-            let operator_or_literal = pair.next().unwrap();
-            match operator_or_literal.as_rule() {
+            let token = pair.next().unwrap();
+            match token.as_rule() {
                 Rule::unary_operator => {
-                    let operator = get_node_from_unary_operator(operator_or_literal.as_str());
-                    let sub_expression = build_ast_from_expression(pair.next().unwrap());
-                    ASTNode::Expression {
+                    let operator = get_node_from_unary_operator(token.as_str());
+                    let next = pair.next().unwrap();
+                    let sub_expression = build_ast_from_expression(&mut pair, next, None);
+                    ASTNode::Factor {
                         operator: Some(operator),
                         expression: Some(Box::new(sub_expression)),
                         literal: None
@@ -73,17 +104,72 @@ fn build_ast_from_expression(pair: pest::iterators::Pair<Rule>) -> ASTNode {
                 },
 
                 Rule::number=> {
-                    let literal = ASTNode::Integer(convert_str_to_int(operator_or_literal.as_str()));
-                    ASTNode::Expression {
+                    let literal = ASTNode::Integer(convert_str_to_int(token.as_str()));
+                    ASTNode::Factor {
                         operator: None,
                         expression: None,
                         literal: Some(Box::new(literal))
                     }
                 },
 
-                unknown_expr => panic!("Unexpected expression: {:?}", unknown_expr),
+                unknown_expr => panic!("Unexpected token: {:?}", unknown_expr),
             }
         },
+
+        unknown_expr => panic!("Unexpected factor: {:?}", unknown_expr),
+    }
+}
+
+
+fn build_ast_from_term(pair: pest::iterators::Pair<Rule>) -> ASTNode {
+    match pair.as_rule() {
+        Rule::term => {
+            let term = pair.into_inner().next().unwrap();
+            ASTNode::Term {
+                lhs: Box::new(build_ast_from_factor(term)),
+                operator: None,
+                rhs: None
+            }
+        },
+
+        unknown_expr => panic!("Unexpected term: {:?}", unknown_expr),
+    }
+}
+
+
+fn build_ast_from_expression(parent:&mut pest::iterators::Pairs<Rule>, pair: pest::iterators::Pair<Rule>, sub_expr:Option<Box<ASTNode>>) -> ASTNode {
+    match pair.as_rule() {
+        Rule::expression => {
+            let mut parent = pair.into_inner();
+            let pair = parent.next().unwrap();
+            build_ast_from_expression(&mut parent, pair, None)
+        },
+
+        Rule::term => {
+            match parent.peek() {
+                Some(token) => {
+                    match token.as_rule() {
+                        Rule::add_or_sub => {
+                            let next_op_token = parent.next().unwrap();
+                            let next_int_token = parent.next().unwrap();
+                            let operator = get_node_from_binary_operator(next_op_token.as_str());
+                            let sub = ASTNode::Expression {
+                                lhs: sub_expr.unwrap_or(Box::new(build_ast_from_term(pair))),
+                                operator: Some(operator),
+                                rhs: Some(Box::new(build_ast_from_term(next_int_token.clone())))
+                            };
+
+                            build_ast_from_expression(parent, next_int_token, Some(Box::new(sub)))
+                        },
+
+                        unknown_expr => panic!("Unexpected token: {:?}", unknown_expr),
+                    }
+                },
+
+                None => *sub_expr.unwrap() // last term in the expression
+            }
+        },
+
         unknown_expr => panic!("Unexpected expression: {:?}", unknown_expr),
     }
 }
@@ -94,7 +180,7 @@ fn build_ast_from_stmt(pair: pest::iterators::Pair<Rule>) -> ASTNode {
         Rule::return_stmt => {
             let mut pair = pair.into_inner();
             let expression = pair.next().unwrap();
-            ASTNode::ReturnStatement(Box::new(build_ast_from_expression(expression)))
+            ASTNode::ReturnStatement(Box::new(build_ast_from_expression(&mut pair, expression, None)))
         },
         unknown_expr => panic!("Unexpected statement construct: {:?}", unknown_expr),
     }
@@ -109,8 +195,7 @@ pub fn build_ast_from_toplvl(pair: pest::iterators::Pair<Rule>) -> ASTNode {
             let identifier = pair.next().unwrap().as_str().to_owned();
             let mut statements:Vec<ASTNode> = Vec::new();
             
-            #[allow(while_true)]
-            while true {
+            loop {
                 match pair.next() {
                     Some(statement) => {
                         statements.push(build_ast_from_stmt(statement));
@@ -138,6 +223,7 @@ pub fn parse(source: &str) -> Result<Vec<ASTNode>, Error<Rule>> {
     let mut ast = vec![];
 
     let pairs = CParser::parse(Rule::program, source)?;
+    // println!("{:#?}", pairs);
     for pair in pairs {
         match pair.as_rule() {
             Rule::function => {
